237a84e03cbd8ec8a274ca4ca8d9898cdd6f7ba2
VictorHugoBTaborda <victor.biscaia.taborda@escola.pr.gov.br>
2025-10-17 07:55:44 -0300

    index.html

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,16 @@
-var titulo = (document.querySelector(".titulo"));
-
+var titulo = document.querySelector(".titulo");
 titulo.textContext = "Aparecida Nutricionista";
+
+
+var paciente = document.querySelector("#primeiro-paciente);
+
+var tdPeso = paciente.querySelector(".info-peso");
+
+var peso = tdPeso.textContext
+
+var tdAltura = paciente.querySelector("info-altura");
+
+var altura = tdAltura.textContent;
+
+var imc = peso / altura * altura;
+console.log (imc);
